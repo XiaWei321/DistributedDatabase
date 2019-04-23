bbc50f0e2ea8849d77dea678105c447302cf3b65
Yifan Hu <huyifan01@corp.netease.com>
2019-04-23 14:13:15 +0800

    fixbug:修改dockerfile文件

--- conf/start-etherem.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-./geth --datadir chain  --rpc --rpcapi "db,web3,eth,net,personal" --rpcaddr "0.0.0.0" --rpccorsdomain "*" --nodiscover --port 16333 --rpcport 8545  console
--- conf/start-redis.sh
+++ conf/start-redis.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-systemctl restart redis
+systemctl start redis
